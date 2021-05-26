5fcc7d2bab2b15dce129ad956a80bb22a516de07
Alessandro Budroni <budroni.alessandro@gmail.com>
2021-05-26 14:42:18 +0200

    Fix test

--- test/test_smooth_lms3_meta1_LF1_fwht_bruteforce_10_101_005.c
+++ test/test_smooth_lms3_meta1_LF1_fwht_bruteforce_10_101_005.c
@@ -44,7 +44,6 @@
 
 #define NUM_REDUCTION_STEPS 3
 #define BRUTE_FORCE_POSITIONS 1
-#define NUMTHREADS 2
 
 int main()
 {
@@ -223,7 +222,7 @@ int main()
     timeStamp(start);
     printf("Solving phase - brute force from positions %d to %d - Fast Walsh Hadamard Transform from positions %d to %d\n", 0, bruteForcePositions-1, bruteForcePositions, bruteForcePositions+fwht_positions-1);
 
-    ret = solve_fwht_search_bruteforce(srcFolderName, binary_solution, bf_solution, zeropositions, bruteForcePositions, fwht_positions, start, NUMTHREADS);
+    ret = solve_fwht_search_bruteforce(srcFolderName, binary_solution, bf_solution, zeropositions, bruteForcePositions, fwht_positions, start);
     if(ret)
     {
         printf("error %d in solve_fwht_search_hybrid\n", ret);
